ba0ac3450186372f77f30fc36028f09d16b09649
grose1 <grantrose1@gmail.com>
2024-02-20 20:09:13 -0500

    Update bluetuskcli.py

--- bluetuskcli.py
+++ bluetuskcli.py
@@ -1,27 +1,101 @@
 # a simple python script to post text to Mastodon and Bluesky at the same time.
-# use pip install Mastodon.py for the mastodon package 
-# use pip install atprototools for the bluesky package
-
-from atprototools import Session
+# a simple python script to post text to Mastodon and Bluesky at the same time.
+from configparser import ConfigParser
+from atproto import Client
 from mastodon import Mastodon
 
-vartext = input('Post Your Post: ')
-post = vartext
-char = (len(post))
-print('Character limit 300 , Current Count: ', char)
-if char > 300:
-    print('Character Limit Exceeded! Try Again...')
-    exit()
-input("Press Enter to continue...")
+# Read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")
 
-# Bluesky login (uses app password)
-USERNAME = "bluesky_handle"
-PASSWORD = "app_password"
-# make a text post in bluesky
-session = Session(USERNAME, PASSWORD)
-resp = session.postBloot(post)
+# Get info from Config File
 
-# post to mastodon (need to create app key first)
+# Bluesky
+bluesky = config_object["BLUESKY"]
+bsky_usr = bluesky["username"]
+bsky_pass = bluesky["app_password"]
+bsky_limit = int(bluesky["char_limit_bsky"])
 
-mastodon = Mastodon(api_base_url = 'mastodon_instance', access_token = 'access_token')
-mastodon.toot(post)
+# Mastodon
+masto = config_object["MASTODON"]
+masto_inst = masto["instance"]
+masto_token = masto["access_token"]
+masto_limit = int(masto["char_limit_masto"])
+
+
+# Post to Mastodon
+def masto():
+    masto_text = input('Toot Your Toot: ')
+    post = masto_text
+    char = (len(post))
+    print('Character limit Mastodon:', masto_limit)
+    print('Current Count:', char)
+    if char > masto_limit:
+        print('Character Limit Exceeded! Try Again...')
+        masto()
+    mastodon = Mastodon(api_base_url=masto_inst, access_token=masto_token)
+    mastodon.toot(post)
+    main()
+
+
+# Post to Bluesky
+def bsky():
+    bsky_text = input('Skeet Your Skeet: ')
+    post = bsky_text
+    char = (len(post))
+    print('Character limit Bluesky:', bsky_limit)
+    print('Current Count:', char)
+    if char > bsky_limit:
+        print('Character Limit Exceeded! Try Again...')
+        bsky()
+    # make a text post in bluesky
+    client = Client()
+    client.login(bsky_usr, bsky_pass)
+
+    client.send_post(text=post)
+    main()
+
+
+# Crosspost to Both
+def crosspost():
+    vartext = input('Post Your Post: ')
+    post = vartext
+    char = (len(post))
+    print('Character limit Bluesky:', bsky_limit)
+    print('Character limit Mastodon:', masto_limit)
+    print('Current Count:', char)
+    if char > masto_limit:
+        print('Character Limit Exceeded! Try Again...')
+        crosspost()
+    if char > bsky_limit:
+        print('Character Limit Exceeded! Try Again...')
+        crosspost()
+    main()
+
+    # make a text post in bluesky
+    client = Client()
+    client.login(bsky_usr, bsky_pass)
+    client.send_post(text=post)
+
+    # post to mastodon
+    mastodon = Mastodon(api_base_url=masto_inst, access_token=masto_token)
+    mastodon.toot(post)
+
+
+# Main Menu
+def main():
+    print('Select 1 to post to Mastodon')
+    print('Select 2 to post to Bluesky')
+    print('Select 3 to post to Both')
+
+    select = input('Choose an Option: ')
+    if select == '1':
+        masto()
+    if select == '2':
+        bsky()
+    if select == '3':
+        crosspost()
+
+
+if __name__ == "__main__":
+    main()
